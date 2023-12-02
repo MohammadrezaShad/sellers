c0d4f094d4b4e01ef10e151bd1779d74301ce1bb
mostafa <mostafaamorovvati@gmail.com>
2023-12-02 23:12:03 +0330

    refactor : cleanup code

--- src/modules/auth/query/pass-recovery-with-phone/pass-recovery-with-phone.handler.ts
+++ src/modules/auth/query/pass-recovery-with-phone/pass-recovery-with-phone.handler.ts
@@ -5,6 +5,7 @@ import { OtpModel } from '../../components/otp/model/otp.model';
 import { FindOtpByPhoneQuery } from '../../components/otp/query/find-otp-by-phone/find-otp-by-phone.query';
 import { PassRecoveryOutput } from '../../dto/pass-recovery.dto';
 import { PassRecoveryWithPhoneQuery } from './pass-recovery-with-phone.query';
+import { ENTERED_CODE_IS_INCORRECT } from '../../constants/error-message.constant';
 
 @QueryHandler(PassRecoveryWithPhoneQuery)
 export class PassRecoveryWithPhoneHandler
@@ -20,7 +21,8 @@ export class PassRecoveryWithPhoneHandler
       new FindOtpByPhoneQuery(phone),
     );
 
-    if (code !== otp.getCode()) throw new BadRequestException('bad code');
+    if (code !== otp?.getCode())
+      throw new BadRequestException(ENTERED_CODE_IS_INCORRECT);
 
     return {
       success: true,
